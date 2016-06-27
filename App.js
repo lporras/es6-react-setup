88ffc9a295a736a82281c8957a1dff3d0e5ad68e
Luis Porras <lporras16@gmail.com>
2016-06-27 20:46:24 -0300

    React Fundamentals: State Basics

--- App.js
+++ App.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        txt: 'this is the state txt',
+        cat: 0
+      };
+
+    }
+    update(e) {
+      this.setState({txt: e.target.value})
+    }
     render() {
-      let txt = this.props.txt;
       return (
         <div>
-          <h1>{txt}</h1>
+          <input type='text' onChange={this.update.bind(this)}/>
+          <h1>{this.state.txt}</h1>
+          <p>{this.state.cat}</p>
           <b>bold</b>
         </div>
       );
