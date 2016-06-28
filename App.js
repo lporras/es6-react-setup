87a08292b840b200f5f53c605994ac0872b00246
Luis Porras <lporras16@gmail.com>
2016-06-27 22:13:51 -0300

    React Fundamentals: Accessing Child Properties

--- App.js
+++ App.js
@@ -2,65 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    constructor() {
-      super();
-      this.state = {
-        red: 0,
-        green: 0,
-        blue: 0
-      };
-      this.update = this.update.bind(this);
-    }
-    update(e) {
-      this.setState({
-        red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
-        green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
-        blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
-      })
-    }
-    render() {
-      return (
-        <div>
-          <Slider ref="red" update={this.update} />
-          {this.state.red}
-          <br />
-          <Slider ref="green" update={this.update} />
-          {this.state.green}
-          <br />
-          <Slider ref="blue" update={this.update} />
-          {this.state.blue}
-          <br />
-        </div>
-      );
-    }
+  render() {
+    return <Button>I <Heart/> React</Button>
+  }
 }
 
-
-class Slider extends React.Component {
-    render() {
-      return (
-        <div>
-          <input ref="inp" type='range'
-            min="0"
-            max="255"
-            onChange={this.props.update}/>
-        </div>
-      );
-    }
+class Button extends React.Component {
+  render() {
+    return <button>{this.props.children}</button>
+  }
 }
 
-App.propTypes = {
-  txt: React.PropTypes.string
-}
-
-App.defaultProps = {
-  txt: 'this is the default text'
-}
-
-//const App = () => <h1>Hello Eggheads</h1>
-//export default App;
+const Heart = () => <span className="glyphicon glyphicon-heart"></span>
 
 ReactDOM.render(
-  <App cat={5}/>,
+  <App />,
   document.getElementById('app')
 )
