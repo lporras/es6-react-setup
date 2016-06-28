358a89b2dbe7891ed3b07e76c9938752466367e7
Luis Porras <lporras16@gmail.com>
2016-06-28 00:13:46 -0300

    React Fundamentals: Build a JSX Live Compiler

--- App.js
+++ App.js
@@ -5,30 +5,40 @@ class App extends React.Component {
     constructor() {
       super();
       this.state = {
-        data: [
-          {id: 1, name: 'Simon Bailey'}, {id: 2, name: 'Thomas Burleson'},
-          {id: 3, name: 'Luis Porras'}, {id: 4, name: 'Elvira Paez'},
-          {id: 5, name: 'Luis Porras'}, {id: 6, name: 'Tainy Gomez'},
-          {id: 7, name: 'Luis Porras'}, {id: 8, name: 'Kelly Bravo'},
-          {id: 9, name: 'Luis Porras'}, {id: 10, name: 'Kelly Bravo'}
-        ]
+        input: '/* add your input here */',
+        output: '',
+        err: ''
+      }
+      this.update = this.update.bind(this)
+    }
+    update(e) {
+      let code = e.target.value;
+      try{
+        this.setState({
+          output: babel.transform(code, {
+            stage: 0,
+            loose: 'all'
+          }).code,
+          err: ''
+        })
+      }catch(err) {
+        this.setState({err: err.message});
       }
     }
     render() {
-      let rows = this.state.data.map( person => {
-        return <PersonRow key={person.id} data={person} />
-      });
-      return <table>
-        <tbody>{rows}</tbody>
-      </table>
+      return (
+        <div>
+          <header>{this.state.err}</header>
+          <div className="container">
+            <textarea
+              onChange={this.update}
+              defaultValue={this.state.input}>
+            </textarea>
+            <pre>{this.state.output}</pre>
+          </div>
+        </div>
+      );
     }
-}
-
-const PersonRow = (props) => {
-  return <tr>
-    <td>{props.data.id}</td>
-    <td>{props.data.name}</td>
-  </tr>
 }
 
 ReactDOM.render(
